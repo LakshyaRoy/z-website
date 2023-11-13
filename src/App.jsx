cb8c60fb81c917f6d5ab750583ebedd4607cc1b2
lakshya <lakshyaroy848@gmail.com>
2023-11-13 23:57:02 +0530

    added home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { NAV_LINKS } from "../Contants";
+// import { useEffect } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path={"/"} element={""} />
+        <Route path={"/"} element={<Home />} />
         <Route path={"/shop"} element={"/shop"} />
         <Route path={"/pages"} element={"/pages"} />
         <Route path={"/blog"} element={"/blog"} />
